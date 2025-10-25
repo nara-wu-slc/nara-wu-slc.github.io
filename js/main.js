a26043fea620ca20b4b2e64cf7dcecede2f245fa
Katsuhito Sudoh <sudoh@ics.nara-wu.ac.jp>
2025-10-25 12:13:51 +0900

    research introduction (only in Japanese)

--- js/main.js
+++ js/main.js
@@ -6410,7 +6410,7 @@ limitations under the License.
           }
 
           var query = $(this).val();
-          var searchPage = 'http://localhost:1313/~sudoh/nara-wu-slc.jp/search/?q=' + query;
+          var searchPage = 'http://localhost:1313/search/?q=' + query;
           document.location = searchPage;
 
           return false;
